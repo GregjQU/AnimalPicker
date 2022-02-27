56c20284c82eba6adef896ac89f293aad2ec7f29
GregjQU <gjeanbaptiste@qu.edu>
2022-02-27 18:08:09 -0500

    adding statement.

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,15 +1,21 @@
-
+import java.util.Scanner;
 public class AnimalPicker {
 
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	Scanner scanner = new Scanner(System.in);	
 		
 		
 		
-		
-System.out.println("which animal do you want to see");
+System.out.println("which animal do you want to see: a dog or a cat");
+String userInput = scanner.nextLine();
+
+if(userInput.toLowerCase() == "dog") {
+	System.out.println("placeholder text");
+	System.out.println(userInput);
+}
 
 
 
