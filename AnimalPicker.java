1c448b773b3387944efc905f4531ae1a0baa2685
GregjQU <gjeanbaptiste@qu.edu>
2022-02-27 17:08:25 -0500

    added pick animal text.

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,8 +1,17 @@
 
 public class AnimalPicker {
 
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		
+		
+System.out.println("which animal do you want to see");
+
+
 
 	}
 
