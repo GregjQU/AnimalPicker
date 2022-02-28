9503f972fbb3054e55879d91028edf9611bee944
GregjQU <gjeanbaptiste@qu.edu>
2022-02-27 21:25:18 -0500

    adding correct statement.

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -6,15 +6,23 @@ public class AnimalPicker {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	Scanner scanner = new Scanner(System.in);	
-		
-		
-		
+	String userInput = "";	
+			
 System.out.println("which animal do you want to see: a dog or a cat");
-String userInput = scanner.nextLine();
+String userInput1 = scanner.nextLine();
+//userInput.toLowerCase();
 
-if(userInput.toLowerCase() == "dog") {
+
+if(userInput1.equalsIgnoreCase("dog") ) {
 	System.out.println("placeholder text");
-	System.out.println(userInput);
+	//System.out.println(userInput);
+	//System.out.println("Text 1 ");
+	
+}
+else if(userInput1.equalsIgnoreCase("cat")) {
+	
+	System.out.println(" kitty");
+	
 }
 
 
