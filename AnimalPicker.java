302e9d7e538bc3a7ec764aa04485ac1cc3159446
GregjQU <gjeanbaptiste@quinnipiac.edu>
2022-03-28 23:25:58 -0400

    fixed step 1

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -18,13 +18,15 @@ if(userInput1.equalsIgnoreCase("dog") ) {
 }*/
  if(userInput1.equalsIgnoreCase("cat")) {
 	
-	System.out.println("            _       \n   ___ __ _| |_ ___ \n  / __/ _` | __/ __|\n | (_| (_| | |_\\__ \\\n  \\___\\__,_|\\__|___/");
-	
-}else if(userInput1.equalsIgnoreCase("fish")) {
+	//System.out.println("            _       \n   ___ __ _| |_ ___ \n  / __/ _` | __/ __|\n | (_| (_| | |_\\__ \\\n  \\___\\__,_|\\__|___/");
+	System.out.println("CAT ASCII ART HERE");
+	 
+}/*
+ else if(userInput1.equalsIgnoreCase("fish")) {
 	System.out.println("   __ _     _     \n  / _(_)___| |__  \n | |_| / __| \'_ \\ \n |  _| \\__ \\ | | |\n |_| |_|___/_| |_|");
-}
+}*/
 else {
-	
+
 	System.out.println("Sorry please enter one of the  options");
 	
 	//System.out.println("which animal do you want to see: a dog or a cat");
